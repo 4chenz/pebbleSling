8a9ace77651fa7e6e3c0fbf04a0f5a8293bd33a8
4chenz <desu.4chenz@national.shitposting.agency>
2020-10-16 05:23:58 -0600

    initial

---
+++ rimg.js
@@ -0,0 +1,16 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+}
+
+function rimg() {
+	var items = document.querySelectorAll("img");
+	for (var i = items.length; i--;) {
+		var img = items[i]
+		var src = img.src.replace(/.*?:\/\//g, "");
+		img.src= "/splits/"+String(getRandomIntInclusive(0,655)).padStart(4, '0')+".png";;
+	}
+
+}
